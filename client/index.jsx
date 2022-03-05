575a10614bb9490af082821ab568fa5dcd67ce34
Cooljean <Cooljean95@ymail.com>
2022-03-05 19:32:17 +0100

    tilbake til der jeg var med samme feil.

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as ReactDOM from "react-dom";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 
-ReactDOM.render(<h1>Hey</h1>, document.getElementById("app"));
+function FrontPage() {
+  return (
+    <div>
+      <div>
+        <Link to={"/quiz"}>Start quiz here</Link>
+      </div>
+      <div>
+        <Link to={"/answers"}>See your answers</Link>
+      </div>
+    </div>
+  );
+}
+
+function Quiz() {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState();
+
+  const navigate = useNavigate();
+
+  useEffect(async () => {
+    setLoading(true);
+    const res = await fetch("/api/login");
+    setData(await res.json());
+    setLoading(false);
+  });
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!data) {
+    console.log({ data });
+  }
+
+  return (
+    <>
+      <h1>{data.quiz}</h1>
+      <div></div>
+    </>
+  );
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<FrontPage />} />
+        <Route path="/quiz" element={<Quiz />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
--- server/server.js
+++ server/server.js
@@ -1,3 +1,83 @@
 import express from "express";
+import path from "path";
 
 const app = express();
+
+const data = [
+  {
+    category: "food",
+    id: 1,
+    quiz: "what color dose a strawberry have?",
+    answers: {
+      answer_a: "green",
+      answer_b: "blue",
+      answer_c: "read",
+      answer_d: "black",
+    },
+    correct_answers: {
+      answer_a: "false",
+      answer_b: "false",
+      answer_c: "true",
+      answer_d: "false",
+    },
+  },
+  {
+    category: "car",
+    id: 2,
+    quiz: "what color dose a black car have?",
+    answers: {
+      answer_a: "green",
+      answer_b: "blue",
+      answer_c: "read",
+      answer_d: "black",
+    },
+    correct_answers: {
+      answer_a: "false",
+      answer_b: "false",
+      answer_c: "false",
+      answer_d: "true",
+    },
+  },
+  {
+    category: "clothes",
+    id: 1,
+    quiz: "what color dose a jeans have?",
+    answers: {
+      answer_a: "green",
+      answer_b: "blue",
+      answer_c: "read",
+      answer_d: "black",
+    },
+    correct_answers: {
+      answer_a: "false",
+      answer_b: "true",
+      answer_c: "false",
+      answer_d: "false",
+    },
+  },
+];
+
+function randomQuiz() {
+  return data[Math.trunc(Math.random() * data.length)];
+}
+
+app.get("/api/login", (req, res) => {
+  function respond() {
+    const object = randomQuiz();
+    const { answers, quiz } = object;
+    return res.json({ quiz, answers });
+  }
+});
+
+app.use(express.static("../client/dist"));
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api/")) {
+    return res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server started on http://localhost:${server.address().port}`);
+});
